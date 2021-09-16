96def883b0e73f648f4d679aa7bb44df1fb92632
DanielBGC <daniel.borba-@hotmail.com>
2021-09-16 15:54:13 -0300

    Configurando frontend para o Heroku

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 4200;
+
+app.use(express.static(__dirname + '/dist/frontend'))
+
+app.get('/*', (req, res) => {
+    res.sendFile(__dirname + '/dist/frontend/index.html')
+})
+
+app.listen(PORT, () => {
+    console.log('Servidor iniciado na porta ' + PORT);
+});
--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,1 +1,2 @@
 export const ApiUrl = 'http://localhost:3000/api';
+// export const ApiUrl = 'https://backend-alunos.herokuapp.com/api';
